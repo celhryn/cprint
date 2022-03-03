c2b1a1781784899c0f47f6966e710f487639b8b6
celhryn <79858410+celhryn@users.noreply.github.com>
2022-03-03 12:12:39 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from function.options import cprint
+
+if __name__ == "__main__":
+
+	cprint("Red", "Ce message s'affiche en rouge")
+	cprint("blue", "Ce message s'affiche en bleu")
+	cprint("yellow", "Ce message s'affiche en jaune")
+	cprint("PURPLE", "Ce message s'affiche en violet")
+	cprint("cyan", "Ce message s'affiche en cyan")
+	cprint("giskxo","Ce message s'affiche en blanc")
+	cprint("OrAnGe", "Ce message s'affiche en orange")
+	cprint("Green", "Ce message s'affiche en vert")
+	cprint("Ce message s'affiche en blanc")
